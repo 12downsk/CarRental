6e122c79351e61c088a120807373859807636ca8
kylerainey <karnq9@mail.umkc.edu>
2016-03-08 15:00:07 -0600

    Car.java

---
+++ src/carrental/Car.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrental;
+
+/**
+ *
+ * @author Kyle
+ */
+public class Car {
+        private String ID;
+    
+    public Car(String ID){
+        this.ID = ID;
+     
+    }
+    
+    public String getID(){
+        return this.ID;
+    }
+}
+
